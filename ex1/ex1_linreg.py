9de26967329ffb64afbbc62648056f25d1a141f2
lxl <lixinglu@live.com>
2023-01-31 18:19:04 +0800

    change 2

--- ex1/ex1_linreg.py
+++ ex1/ex1_linreg.py
@@ -9,11 +9,11 @@ data = np.loadtxt("ex1/housing.data")
 #print(array1.T)
 data = np.c_[np.ones(data.shape[0]),data]  #add one column of ones to the data as an additional intercept feature.
 print(data[-1])
-np.random.shuffle(data) #随机打乱数组顺序，如果是多维数组，打乱第一维
+np.random.shuffle(data) #随机打乱数组顺序，如果是多维数组，numpy.random.shuffle会打乱矩阵的第一维
 print(data[-1])
-Train_X = data[:400, :-2]
-Train_y = data[ :400, -1]
-Test_X = data[400: , :-2]
+Train_X = data[:400, :-1] #
+Train_y = data[ :400, -1] #
+Test_X = data[400: , :-1]
 Test_y = data[400: , -1]
 print(Test_y)
 print(Test_y.shape)
@@ -21,3 +21,4 @@ print(Train_y.shape)
 print(data.shape[0])
 m = Train_X.shape[0]
 n = Train_X.shape[1]
+theta = np.random.rand(n)
